1dbb2ba7f703efe080b1b54400c93774af2a9d69
omrinao <omrinao@post.bgu.ac.il>
2018-06-13 21:12:34 +0300

    view controller

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -3,15 +3,23 @@ package View;
 import Server.Server;
 import ViewModel.MyViewModel;
 import algorithms.search.Solution;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import javafx.scene.control.DialogPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 
@@ -35,32 +43,40 @@ public class MyViewController implements IView, Observer {
 
     public void SetStageAboutEvent(ActionEvent actionEvent) {
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        DialogPane dialogPane = alert.getDialogPane();
-        dialogPane.getStylesheets().add(
-                getClass().getResource("ViewStyle.css").toExternalForm());
-        dialogPane.getStyleClass().add("myDialog");
-        alert.setContentText("Welcome to our maze application!" + '\n' + "In this application you can generate a random maze and try to solve it your own, or you can ask the application to do it." + '\n'
-        + "Maze generation made by Prim's algorithm and solved by DFS algorithm." + '\n' + "The programmers behind this application:" + '\n' + "Hagai Kalinhoff and Omri Naor");
-        alert.setHeaderText("                    About");
-        alert.setTitle("About");
-        Optional<ButtonType> result = alert.showAndWait();
+        try {
+            Stage stage = new Stage();
+            stage.setTitle("About");
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent root = fxmlLoader.load(getClass().getResource("About.fxml").openStream());
+            Scene scene = new Scene(root, 400, 350);
+            scene.getStylesheets().add(getClass().getResource("About.css").toExternalForm());
+            stage.setScene(scene);
+            AboutController a = fxmlLoader.getController();
+            stage.initModality(Modality.APPLICATION_MODAL); //Lock the window until it closes
+            stage.show();
+        } catch (Exception e) {
+
+        }
     }
 
     public void SetStageHelpEvent(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        DialogPane dialogPane = alert.getDialogPane();
-        dialogPane.getStylesheets().add(
-                getClass().getResource("ViewStyle.css").toExternalForm());
-        dialogPane.getStyleClass().add("myDialog");
-        alert.setContentText("Maze Rules:" + '\n' + "1. The character can be moved only to empty cells (non-wall cells)" + '\n' +
+        String content = "Maze Rules:" + '\n' + "1. The character can be moved only to empty cells (non-wall cells)" + '\n' +
                 "2. In order to solve the maze, you need to reach the goal cell" + '\n' + '\n' +
-        "Game Instructions:" + '\n' + "Use the NumPad numbers to move the character:" + '\n' +
-        "UP - 8" + '\n' + "DOWN - 2" + '\n' + "RIGHT - 6" + '\n' + "LEFT - 4" + '\n'
-        + "Diagonal Moves:" + '\n' + "UP-LEFT - 7" + '\n' + "DOWN-LEFT - 1" + '\n' + "UP-RIGHT - 9" + '\n' + "DOWN-RIGHT - 3" + '\n');
+                "Game Instructions:" + '\n' + "Use the NumPad numbers to move the character:" + '\n' +
+                "UP - 8       DOWN - 2" + '\n' + "RIGHT - 6       LEFT - 4" + '\n'
+                + "Diagonal Moves:" + '\n' + "UP-LEFT - 7       DOWN-LEFT - 1" + '\n' + "UP-RIGHT - 9       DOWN-RIGHT - 3" + '\n';
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setContent(new Label(content));
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setMinHeight(Region.USE_PREF_SIZE);
+        dialogPane.getStylesheets().add(
+                getClass().getResource("Help.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        alert.setResizable(true);
         alert.setHeaderText("                    Help");
         alert.setTitle("Help");
-        Optional<ButtonType> result = alert.showAndWait();
+
+        alert.show();
     }
 
     public void SetStagePropertiesEvent(ActionEvent actionEvent) {
